30b4250cc30d453109b363455e863fc42ee8cddb
noname <jm080265@gmail.com>
2022-01-31 00:00:19 +0900

    31th finish

--- nextjs-app/pages/index.js
+++ nextjs-app/pages/index.js
@@ -1,4 +1,5 @@
 import Head from 'next/head'
+import Link from 'next/link'
 
 export default function Home() {
   return (
@@ -10,8 +11,11 @@ export default function Home() {
 
       <main>
         <h1 className="title">
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
-        </h1>
+  Read{' '}
+  <Link href="/posts/first-post">
+    <a>this page!</a>
+  </Link>
+</h1>
 
         <p className="description">
           Get started by editing <code>pages/index.js</code>
