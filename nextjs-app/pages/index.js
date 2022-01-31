0dce8a54942f0e85137c0165f212354b7babc9aa
noname <jm080265@gmail.com>
2022-01-31 21:11:39 +0900

    チュートリアル

--- nextjs-app/pages/index.js
+++ nextjs-app/pages/index.js
@@ -59,7 +59,7 @@ export default function Home() {
           rel="noopener noreferrer"
         >
           Powered by{' '}
-          <img src="/vercel.svg" alt="Vercel" className="logo" />
+          <img src="/img/vercel.svg" alt="Vercel" className="logo" />
         </a>
       </footer>
 
